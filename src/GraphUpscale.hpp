aba2f1bea61ff158c59a804f9ac0667db282b262
Stephan Gelever <gelever@pdx.edu>
2018-02-02 14:14:14 -0800

    Fix copyright

--- src/GraphUpscale.hpp
+++ src/GraphUpscale.hpp
@@ -1,17 +1,17 @@
-/*bheader**********************************************************************
+/*BHEADER**********************************************************************
  *
- * copyright (c) 2017,  lawrence livermore national security, llc.
- * produced at the lawrence livermore national laboratory.
- * llnl-code-xxxxxx. all rights reserved.
+ * Copyright (c) 2018, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ * LLNL-CODE-745247. All Rights reserved. See file COPYRIGHT for details.
  *
- * this file is part of smoothg.  see file copyright for details.
- * for more information and source code availability see xxxxx.
+ * This file is part of smoothG. For more information and source code
+ * availability, see https://www.github.com/llnl/smoothG.
  *
- * smoothg is free software; you can redistribute it and/or modify it under the
- * terms of the gnu lesser general public license (as published by the free
- * software foundation) version 2.1 dated february 1999.
+ * smoothG is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License (as published by the Free
+ * Software Foundation) version 2.1 dated February 1999.
  *
- ***********************************************************************eheader*/
+ ***********************************************************************EHEADER*/
 
 /** @file
 
