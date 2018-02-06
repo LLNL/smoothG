b3f10475fc70be555c4617742ef358f679890d40
Stephan Gelever <gelever@pdx.edu>
2018-02-06 08:12:55 -0800

    Revert cmake module files

--- cmake/modules/CMakeUtilities.cmake
+++ cmake/modules/CMakeUtilities.cmake
@@ -1,11 +1,11 @@
 # BHEADER ####################################################################
 #
-# Copyright (c) 2017,  Lawrence Livermore National Security, LLC.
+# Copyright (c) 2018, Lawrence Livermore National Security, LLC.
 # Produced at the Lawrence Livermore National Laboratory.
-# LLNL-CODE-XXXXXX. All Rights reserved.
+# LLNL-CODE-745247. All Rights reserved. See file COPYRIGHT for details.
 #
-# This file is part of smoothG.  See file COPYRIGHT for details.
-# For more information and source code availability see XXXXX.
+# This file is part of smoothG. For more information and source code
+# availability, see https://www.github.com/llnl/smoothG.
 #
 # smoothG is free software; you can redistribute it and/or modify it under the
 # terms of the GNU Lesser General Public License (as published by the Free
