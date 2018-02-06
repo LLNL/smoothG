a3ee50cc08162c339e47341149601958cfce1520
Chak Shing Lee <lee1029@llnl.gov>
2018-02-06 12:15:14 -0800

    define trace_method explicitly in lineargraph.cpp

--- testcode/lineargraph.cpp
+++ testcode/lineargraph.cpp
@@ -201,6 +201,7 @@ int main(int argc, char* argv[])
     const int num_partitions = 2;
     const int max_evects = 1;
     const double spect_tol = 0.0;
+    const double trace_method = 1;
     const double test_tol = 1.e-8;
     args.Parse();
     if (myid == 0)
@@ -224,7 +225,7 @@ int main(int argc, char* argv[])
     std::vector<mfem::DenseMatrix> local_spectral_vertex_targets;
 
     LocalMixedGraphSpectralTargets localtargets(
-        spect_tol, max_evects, 1, graph.GetM(), graph.GetD(), graph_topology);
+        spect_tol, max_evects, trace_method, graph.GetM(), graph.GetD(), graph_topology);
 
     localtargets.Compute(local_edge_traces, local_spectral_vertex_targets);
 
