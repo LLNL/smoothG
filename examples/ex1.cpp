ee1c1a93a7edb42dd7db11d86e6f8d384994ba47
Stephan Gelever <gelever@pdx.edu>
2018-03-02 13:09:32 -0800

    Make style

--- examples/ex1.cpp
+++ examples/ex1.cpp
@@ -54,88 +54,88 @@ int main(int argc, char* argv[])
     mfem::OptionsParser args(argc, argv);
     int coarse_factor = 12;
     args.AddOption(&coarse_factor, "-np", "--num-part",
-            "Coarsening factor");
+                   "Coarsening factor");
     const char* graphFileName = "../../graphdata/vertex_edge_sample.txt";
     args.AddOption(&graphFileName, "-g", "--graph",
-            "File to load for graph connection data.");
+                   "File to load for graph connection data.");
     const char* FiedlerFileName = "../../graphdata/fiedler_sample.txt";
     args.AddOption(&FiedlerFileName, "-f", "--fiedler",
-            "File to load for the Fiedler vector.");
+                   "File to load for the Fiedler vector.");
     const char* partition_filename = "../../graphdata/partition_sample.txt";
     args.AddOption(&partition_filename, "-p", "--partition",
-            "Partition file to load (instead of using metis).");
+                   "Partition file to load (instead of using metis).");
     const char* weight_filename = "";
     args.AddOption(&weight_filename, "-w", "--weight",
-            "File to load for graph edge weights.");
+                   "File to load for graph edge weights.");
     const char* w_block_filename = "";
     args.AddOption(&w_block_filename, "-wb", "--w_block",
-            "File to load for w block.");
+                   "File to load for w block.");
     bool metis_agglomeration = false;
     args.AddOption(&metis_agglomeration, "-ma", "--metis-agglomeration",
-            "-nm", "--no-metis-agglomeration",
-            "Use Metis as the partitioner (instead of loading partition).");
+                   "-nm", "--no-metis-agglomeration",
+                   "Use Metis as the partitioner (instead of loading partition).");
     int max_evects = 4;
     args.AddOption(&max_evects, "-m", "--max-evects",
-            "Maximum eigenvectors per aggregate.");
+                   "Maximum eigenvectors per aggregate.");
     double spect_tol = 1.e-3;
     args.AddOption(&spect_tol, "-t", "--spect-tol",
-            "Spectral tolerance for eigenvalue problems.");
+                   "Spectral tolerance for eigenvalue problems.");
     bool hybridization = false;
     args.AddOption(&hybridization, "-hb", "--hybridization", "-no-hb",
-            "--no-hybridization", "Enable hybridization.");
+                   "--no-hybridization", "Enable hybridization.");
     bool generate_graph = false;
     args.AddOption(&generate_graph, "-gg", "--generate-graph", "-no-gg",
-            "--no-generate-graph", "Generate a graph at runtime.");
+                   "--no-generate-graph", "Generate a graph at runtime.");
     bool generate_fiedler = false;
     args.AddOption(&generate_fiedler, "-gf", "--generate-fiedler", "-no-gf",
-            "--no-generate-fiedler", "Generate a fiedler vector at runtime.");
+                   "--no-generate-fiedler", "Generate a fiedler vector at runtime.");
     bool save_fiedler = false;
     args.AddOption(&save_fiedler, "-sf", "--save-fiedler", "-no-sf",
-            "--no-save-fiedler", "Save a generate a fiedler vector at runtime.");
+                   "--no-save-fiedler", "Save a generate a fiedler vector at runtime.");
     int gen_vertices = 1000;
     args.AddOption(&gen_vertices, "-nv", "--num-vert",
-            "Number of vertices of the graph to be generated.");
+                   "Number of vertices of the graph to be generated.");
     int mean_degree = 40;
     args.AddOption(&mean_degree, "-md", "--mean-degree",
-            "Average vertex degree of the graph to be generated.");
+                   "Average vertex degree of the graph to be generated.");
     double beta = 0.15;
     args.AddOption(&beta, "-b", "--beta",
-            "Probability of rewiring in the Watts-Strogatz model.");
+                   "Probability of rewiring in the Watts-Strogatz model.");
     int seed = 0;
     args.AddOption(&seed, "-s", "--seed",
-            "Seed (unsigned integer) for the random number generator.");
+                   "Seed (unsigned integer) for the random number generator.");
     int isolate = -1;
     args.AddOption(&isolate, "--isolate", "--isolate",
-            "Isolate a single vertex (for debugging so far).");
+                   "Isolate a single vertex (for debugging so far).");
     bool dual_target = false;
     args.AddOption(&dual_target, "-dt", "--dual-target", "-no-dt",
-            "--no-dual-target", "Use dual graph Laplacian in trace generation.");
+                   "--no-dual-target", "Use dual graph Laplacian in trace generation.");
     bool scaled_dual = false;
     args.AddOption(&scaled_dual, "-sd", "--scaled-dual", "-no-sd",
-            "--no-scaled-dual", "Scale dual graph Laplacian by (inverse) edge weight.");
+                   "--no-scaled-dual", "Scale dual graph Laplacian by (inverse) edge weight.");
     bool energy_dual = false;
     args.AddOption(&energy_dual, "-ed", "--energy-dual", "-no-ed",
-            "--no-energy-dual", "Use energy matrix in trace generation.");
+                   "--no-energy-dual", "Use energy matrix in trace generation.");
 
 
     // MFEM Options
-    const char *mesh_file = "../data/star.mesh";
+    const char* mesh_file = "../data/star.mesh";
     args.AddOption(&mesh_file, "-mesh", "--mesh",
-            "Mesh file to use.");
+                   "Mesh file to use.");
     int order = 1;
     args.AddOption(&order, "-o", "--order",
-            "Finite element order (polynomial degree) or -1 for"
-            " isoparametric space.");
+                   "Finite element order (polynomial degree) or -1 for"
+                   " isoparametric space.");
     int ref_levels = 1;
     args.AddOption(&ref_levels, "-nr", "--num_refine",
-            "Number of times to refine mesh.");
+                   "Number of times to refine mesh.");
     bool static_cond = false;
     args.AddOption(&static_cond, "-sc", "--static-condensation", "-no-sc",
-            "--no-static-condensation", "Enable static condensation.");
+                   "--no-static-condensation", "Enable static condensation.");
     bool visualization = 1;
     args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
-            "--no-visualization",
-            "Enable or disable GLVis visualization.");
+                   "--no-visualization",
+                   "Enable or disable GLVis visualization.");
 
     args.Parse();
     if (!args.Good())
@@ -152,7 +152,7 @@ int main(int argc, char* argv[])
         args.PrintOptions(std::cout);
     }
 
-    Mesh *mesh = new Mesh(mesh_file, 1, 1);
+    Mesh* mesh = new Mesh(mesh_file, 1, 1);
     int dim = mesh->Dimension();
 
     for (int l = 0; l < ref_levels; l++)
@@ -160,7 +160,7 @@ int main(int argc, char* argv[])
         mesh->UniformRefinement();
     }
 
-    FiniteElementCollection *fec;
+    FiniteElementCollection* fec;
     if (order > 0)
     {
         fec = new H1_FECollection(order, dim);
@@ -173,7 +173,7 @@ int main(int argc, char* argv[])
     {
         fec = new H1_FECollection(order = 1, dim);
     }
-    FiniteElementSpace *fespace = new FiniteElementSpace(mesh, fec);
+    FiniteElementSpace* fespace = new FiniteElementSpace(mesh, fec);
 
     Array<int> ess_tdof_list;
     if (mesh->bdr_attributes.Size())
@@ -183,7 +183,7 @@ int main(int argc, char* argv[])
         fespace->GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
     }
 
-    LinearForm *b = new LinearForm(fespace);
+    LinearForm* b = new LinearForm(fespace);
     ConstantCoefficient one(1.0);
     b->AddDomainIntegrator(new DomainLFIntegrator(one));
     b->Assemble();
@@ -191,7 +191,7 @@ int main(int argc, char* argv[])
     GridFunction x(fespace);
     x = 0.0;
 
-    BilinearForm *a = new BilinearForm(fespace);
+    BilinearForm* a = new BilinearForm(fespace);
     a->AddDomainIntegrator(new DiffusionIntegrator(one));
 
     if (static_cond) { a->EnableStaticCondensation(); }
@@ -238,8 +238,8 @@ int main(int argc, char* argv[])
     {
         /// [Upscale]
         GraphUpscale upscale(comm, vertex_edge_global, global_partitioning,
-                spect_tol, max_evects, dual_target, scaled_dual,
-                energy_dual, hybridization, weight);
+                             spect_tol, max_evects, dual_target, scaled_dual,
+                             energy_dual, hybridization, weight);
 
         upscale.PrintInfo();
         upscale.ShowSetupTime();
