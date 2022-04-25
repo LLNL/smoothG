d71bc9e3420e54958de46f5f12c77e16f172d62b
chakshinglee <chakshinglee@gmail.com>
2022-04-25 17:16:39 +0800

    remove unnecessary stuff in well_basis.cpp

--- examples/well_basis.cpp
+++ examples/well_basis.cpp
@@ -35,35 +35,6 @@
 
 using namespace smoothg;
 
-double mu_o = 3e-3; //0.005; //0.0002; //
-double mu_w = 3e-4; // 0.3*centi*poise //1e-3;
-int relperm_order = 2;
-//mfem::Array<int> well_cells;
-mfem::Array<int> well_perforations;
-mfem::Vector well_cell_gf;
-
-
-enum SteppingScheme { IMPES = 1, SequentiallyImplicit, FullyImplcit };
-
-struct EvolveParamenters
-{
-    double total_time = 10.0;    // Total time
-    double dt = 1.0;   // Time step size
-    int vis_step = 0;
-    SteppingScheme scheme = IMPES;
-};
-
-void SetOptions(FASParameters& param, bool use_vcycle, int num_backtrack, double diff_tol);
-
-mfem::Vector TotalMobility(const mfem::Vector& S);
-mfem::Vector dTMinv_dS(const mfem::Vector& S);
-mfem::Vector FractionalFlow(const mfem::Vector& S);
-mfem::Vector dFdS(const mfem::Vector& S);
-
-DarcyProblem* problem_ptr;
-std::vector<mfem::socketstream> sout_resid_(50); // this should not be needed (only for debug)
-
-
 int main(int argc, char* argv[])
 {
     int num_procs, myid;
@@ -75,55 +46,9 @@ int main(int argc, char* argv[])
     MPI_Comm_rank(comm, &myid);
 
     // program options from command line
-    EvolveParamenters evolve_param;
     mfem::OptionsParser args(argc, argv);
-    std::string base_dir = "/Users/lee1029/Downloads/";
-    const char* problem_dir = "";
-    args.AddOption(&problem_dir, "-pd", "--problem-directory",
-                   "Directory where data files are located");
-    const char* perm_file = "spe_perm.dat";
-    args.AddOption(&perm_file, "-p", "--perm", "SPE10 permeability file data.");
     int dim = 3;
     args.AddOption(&dim, "-d", "--dim", "Dimension of the physical space.");
-    int slice = 0;
-    args.AddOption(&slice, "-s", "--slice", "Slice of SPE10 data for 2D run.");
-    int well_height = 1;
-    args.AddOption(&well_height, "-wh", "--well-height", "Well Height.");
-    double inject_rate = 5.6544e-04;//0.00005;// * 0.6096;
-    args.AddOption(&inject_rate, "-ir", "--inject-rate", "Injector rate.");
-    double bhp = -2.7579e07;//-1.0e6;
-    args.AddOption(&bhp, "-bhp", "--bottom-hole-pressure", "Bottom Hole Pressure.");
-    args.AddOption(&evolve_param.dt, "-dt", "--delta-t", "Time step.");
-    args.AddOption(&evolve_param.total_time, "-time", "--total-time",
-                   "Total time to step.");
-    args.AddOption(&evolve_param.vis_step, "-vs", "--vis-step",
-                   "Step size for visualization.");
-    int scheme = 3;
-    args.AddOption(&scheme, "-scheme", "--stepping-scheme",
-                   "Time stepping: 1. IMPES, 2. sequentially implicit, 3. fully implicit. ");
-    bool use_vcycle = true;
-    args.AddOption(&use_vcycle, "-VCycle", "--use-VCycle", "-FMG",
-                   "--use-FMG", "Use V-cycle or FMG-cycle.");
-    int num_backtrack = 0;
-    args.AddOption(&num_backtrack, "--num-backtrack", "--num-backtrack",
-                   "Maximum number of backtracking steps.");
-    double diff_tol = -1.0;
-    args.AddOption(&diff_tol, "--diff-tol", "--diff-tol",
-                   "Tolerance for coefficient change.");
-    int print_level = -1;
-    args.AddOption(&print_level, "-print-level", "--print-level",
-                   "Solver print level (-1 = no to print, 0 = final error, 1 = all errors.");
-    bool smeared_front = true;
-    args.AddOption(&smeared_front, "-smeared-front", "--smeared-front", "-sharp-front",
-                   "--sharp-front", "Control density to produce smeared or sharp saturation front.");
-    args.AddOption(&relperm_order, "-ro", "--relperm-order",
-                   "Exponent of relperm function.");
-    UpscaleParameters upscale_param;
-    upscale_param.spect_tol = 1.0;
-    upscale_param.max_evects = 1;
-    upscale_param.max_traces = 1;
-    upscale_param.max_levels = 1;
-    upscale_param.RegisterInOptionsParser(args);
     args.Parse();
     if (!args.Good())
     {
@@ -139,15 +64,9 @@ int main(int argc, char* argv[])
         args.PrintOptions(std::cout);
     }
 
-    evolve_param.scheme = static_cast<SteppingScheme>(scheme);
-
     // Setting up finite volume discretization problem
-    unique_ptr<DarcyProblem> problem;
-    problem.reset(new LocalProblem(comm, dim, std::vector<int>(dim, 29)));
-
-    unique_ptr<mfem::Array<int>> partition(nullptr);
-    Upscale upscale(problem->GetFVGraph(true), upscale_param, partition.get(),
-                    &problem->EssentialAttribute());
+    LocalProblem problem(comm, dim, std::vector<int>(dim, 29));
+    Upscale upscale(problem.GetFVGraph(true));
     upscale.PrintInfo();
 
     mfem::BlockVector rhs(upscale.BlockOffsets(0));
@@ -157,14 +76,11 @@ int main(int argc, char* argv[])
 
     mfem::BlockVector sol = upscale.Solve(0, rhs);
 
-//    mfem::Vector p_wc(19);
-//    for (int i = 0; i < 19; ++i) { p_wc[i] = sol.GetBlock(1)[sol.BlockSize(1)-19+i]; }
-//    p_wc.Print();
     std::cout<<"P max: " << sol.GetBlock(1).Max() <<", P min: " << sol.GetBlock(1).Min() <<"\n";
     std::cout<<"P mean: " << sol.GetBlock(1).Sum() / sol.BlockSize(1) <<"\n";
 
     mfem::socketstream vis_v;
-    problem->VisSetup(vis_v, sol.GetBlock(1), 0.0, 0.0, "Pressure");
+    problem.VisSetup(vis_v, sol.GetBlock(1), 0.0, 0.0, "Pressure");
 
     return EXIT_SUCCESS;
 }
